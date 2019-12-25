9f99206637dec9525f2d57624a233c59dd5e45a8
sidashen <shashasi8023@163.com>
2019-12-25 16:47:25 +0800

    [shasha] refeactor: add list function

--- js/index.js
+++ js/index.js
@@ -1,21 +1,33 @@
 var addListBtn = document.getElementsByClassName('add-list-btn');
-addListBtn[0].addEventListener ('click', createToDoList)
+var tasksContent = document.getElementsByClassName('tasks');
+
+addListBtn[0].addEventListener ('click', function (){
+  createToDoList();
+  clearText();
+});
+
+document.onkeydown = function (event) {
+ 
+  if (e && e.keyCode == 13) {
+    event.preventDefault();
+    createToDoList();
+    clearText();
+  }
+}
 
 function createToDoList() {
   var showListContent = document.getElementsByClassName('list-content');
-  var tasksContent = document.getElementsByClassName('tasks');
 
   if (tasksContent[0].value)  {
     var listContent = document.createElement('li');
     listContent.innerHTML = `<input type="checkbox" class="check"/>${tasksContent[0].value}`;
     showListContent[0].appendChild(listContent);
-    console.log(listContent);
   } else {
- return;
+  return;
   }
 }
 
 function clearText() {
-  
+  tasksContent[0].value = '';
 }
 
