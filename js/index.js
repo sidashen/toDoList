41051020ec84e5e2c8d90db2e65276cd53702342
sidashen <shashasi8023@163.com>
2019-12-25 18:37:06 +0800

    [shasha] feat: complete and active functions

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 var addListBtn = document.getElementsByClassName('add-list-btn');
 var tasksContent = document.getElementsByClassName('tasks');
+var showActiveBtn = document.getElementsByClassName('show-active');
+var showCompleteBtn = document.getElementsByClassName('show-complete');
+var showListContent = document.getElementsByClassName('all-list-content');
 
-addListBtn[0].addEventListener ('click', function (){
+addListBtn[0].addEventListener('click', function (){
   createToDoList();
   clearText();
 });
+
+showActiveBtn[0].addEventListener('click', showNewList);
+
+showCompleteBtn[0].addEventListener('click', hideNewList);
 
 document.onkeydown = function (event) {
 
@@ -16,11 +23,10 @@ document.onkeydown = function (event) {
 }
 
 function createToDoList() {
-  var showListContent = document.getElementsByClassName('all-list-content');
 
   if (tasksContent[0].value)  {
     var listContent = document.createElement('li');
-    listContent.innerHTML = `<input type="checkbox" class="check" id="list-content"/><label for="list-content">${tasksContent[0].value}</label>`;
+    listContent.innerHTML = `<input type="checkbox" class="check"/><span>${tasksContent[0].value}</span>`;
     showListContent[0].appendChild(listContent);
   } else {
   return;
@@ -31,4 +37,11 @@ function clearText() {
   tasksContent[0].value = '';
 }
 
+function showNewList() {
+  showListContent[0].style.visibility = 'visible';
+}
+
+function hideNewList() {
+  showListContent[0].style.visibility = 'hidden';
+}
 
