443753e0b14c372a50ef1458243b015d725210c0
sidashen <shashasi8023@163.com>
2019-12-26 13:47:43 +0800

    [shasha] feat: data storage

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ var showActiveBtn = document.getElementsByClassName('show-active');
 var showCompleteBtn = document.getElementsByClassName('show-complete');
 var showListContent = document.getElementsByClassName('all-list-content');
 var showAllListBtn = document.getElementsByClassName('show-all');
+var tasksArr = [];
+localStorage.setItem('toDoList', JSON.stringify(tasksArr));
 
 addListBtn[0].addEventListener('click', function (){
   createToDoList();
@@ -25,6 +27,11 @@ document.onkeydown = function (event) {
 }
 
 function createToDoList() {
+  var selectBtn = document.getElementsByClassName('check-box');
+  var selectBtnArr = Array.from(selectBtn);
+  var key = selectBtnArr.length;
+  var tasksInfo = tasksContent[0].value;
+  var checkStatus = '';
 
   if (tasksContent[0].value)  {
     var listContent = document.createElement('li');
@@ -34,15 +41,17 @@ function createToDoList() {
   return;
   }
 
-  var selectBtn = document.getElementsByClassName('check-box');
-  var selectBtnArr = Array.from(selectBtn);
   selectBtnArr.forEach(item => {
     item.addEventListener('click', function (event) {
       var target = event.target;
       completeList(target);
+      changeCheckStatus(target);
     })
   });
-  // localStorage.setItem('key', JSON.stringify(selectBtnArr));
+
+  tasksDataStorage(key, tasksInfo, checkStatus);
+
+  return selectBtnArr;
 }
 
 function clearText() {
@@ -71,6 +80,51 @@ function completeList(target) {
   }
 }
 
+function changeCheckStatus(target) {
+  if (target.checked) {
+    checkStatus = 'checked';
+  } else {
+    checkStatus = '';
+  }
+  var index = judgeIndex(target.parentNode);
+  tasksArr[index].isChecked = checkStatus;
+  localStorage.setItem('toDoList', JSON.stringify(tasksArr));
+}
+
+function judgeIndex(list) {
+  tasksArr = JSON.parse(localStorage.getItem('toDoList'));
+  var index;
+  tasksArr.forEach(item => {
+    if (item.tasks === list.innerText) {
+      index = item.key;
+    }
+  });
+  return index;
+}
+
+// function showList(status) {
+//   switch(status) {
+//     case ():
+
+//   }
+// }
+
 function showAllList() {
   showListContent[0].style.visibility = 'visible';
 }
+
+function tasksDataStorage(key, tasksInfo, checkStatus) {
+  tasksArr = JSON.parse(localStorage.getItem('toDoList'));
+
+  var tasksObj = {
+    key: key,
+    tasks: tasksInfo,
+    isChecked: checkStatus
+  }
+
+  tasksArr.push(tasksObj);
+  localStorage.setItem('toDoList', JSON.stringify(tasksArr));
+}
+
+
+
